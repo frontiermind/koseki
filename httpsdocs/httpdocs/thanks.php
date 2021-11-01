a43027399d2fb048a3597fdeb1d0a01f3a74ae72
Takara Tomita <t.tomita@fmc-inc.jp>
2021-11-01 19:49:53 +0900

    フォーム作成

---
+++ httpsdocs/httpdocs/thanks.php
@@ -0,0 +1,65 @@
+<!-- saved from url=(0022)http://internet.e-mail -->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<link href="css\style.css" rel="stylesheet" type="text/css">
+<link href="./css/style.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+<meta name="keywords" content="koseki,小関法律事務所,小関,法律事務所,小関勇二,小関健二,新橋法律事務所"/>
+<title>小関法律事務所</title>
+</head>
+<body class="body2 contact">
+
+ <div id="sidebar-left">  
+</div> 
+ <div id="sidebar-right">  
+</div> 
+
+<div class="header">
+
+	<table class="header2">
+		<tr>
+			<td>
+				<a href="/index.html"><img class="half" src="TopName2.png" /></a>
+			</td>
+			<td>
+				<p class="header" >
+					〒105-0004　東京都港区新橋2丁目21番1号　新橋駅前ビル2号館 7階<br/>
+					TEL　 : 03-3574-9401 　　<br/>
+					E-mail: info@koseki-law.jp
+				</p>
+			</td>
+		</tr>
+	</table>
+	
+
+
+
+	<nobr>
+		<a href="index.html#goaisatsu"><img class="quarter" src="goaisatsu_tab2.png"></a>
+		<a href="index.html#bengoshi"><img class="quarter" src="bengoshi_tab2.png"></a>
+		<a href="index.html#access"><img class="quarter" src="access_tab2.png"></a>
+		<a href="index.html#contact"><img class="quarter" src="toiawase_tab2.png"></a>
+	</nobr>
+
+<!-- ここからコンテンツ -->
+
+
+
+  <main class="l-main">
+    <p>お問い合わせありがとうございました。</p>
+    <p><a href="./index.html">TOPへ</a></p>
+  </main>
+
+</div>
+
+  <!-- ここまでコンテンツ -->
+
+  <div class="footer">
+		(C) 2017 小関法律事務所
+</div>
+
+
+
+</body>
+</html>
